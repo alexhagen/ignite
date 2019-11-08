c658889e4c4d35ab7566c14e9c7cc10fa1b65576
Alex Hagen <alexander.hagen@pnnl.gov>
2019-11-08 13:32:24 -0800

    working line plotting and confusion matrix plotting

---
+++ test/test_visdom.py
@@ -0,0 +1,11 @@
+from ignite import vd
+import numpy as np
+import time
+
+x = vd.vd(env="test_vd")
+cm = np.diag(np.ones((5,)))
+x.plot_cm(cm)
+
+for i in range(10):
+    x.plot_metric('loss', [np.power(i, 2.0)])
+    time.sleep(1)
--- vd.py
+++ vd.py
@@ -1,5 +1,5 @@
 import visdom
-
+import matplotlib.pyplot as plt
 '''
 
 This will log from a database/file that we are saving using our "Metrics" class
@@ -9,15 +9,25 @@ appropriate type of plot.
 
 class vd(object):
     def __init__(self, env):
-        self.visdom = visdom.Visdom()
+        self.visdom = visdom.Visdom(server="http://deepard.pnl.gov", env=env)
+        self.steps = dict()
 
-    def plot_cm(self, cm, title='Confusion Matrix'):
+    def plot_cm(self, cm, title='Confusion Matrix', categories=None):
         """Plot the current confusion matrix."""
-        pass
+        size = cm.shape[0]
+        plt.imshow(cm, cmap='cividis', extent=[0, size, 0, size])
+        self.visdom.matplot(plt.gcf())
 
     def plot_metric(self, metric_name, metric, step=None):
         """."""
-        pass
+        if step is None:
+            if metric_name in self.steps.keys():
+                step = self.steps[metric_name] + 1.0
+            else:
+                step = 0.0
+        self.steps[metric_name] = step
+        self.visdom.line(X=[step], Y=metric, name=metric_name, win=metric_name,
+                         update='append')
 
     def plot_image(self, image_name, image):
         """."""
